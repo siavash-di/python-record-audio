41ffde0d71d360d36d978e812cc8faa987bc444a
Max Base <MaxBaseCode@Gmail.Com>
2020-10-31 22:43:11 +0330

    Beauty and adding comment

--- record.py
+++ record.py
@@ -1,27 +1,21 @@
 import pyaudio
 import wave
+
+# Start
 p = pyaudio.PyAudio()
-
-stream = p.open(format=pyaudio.paInt16,
-                channels=2,
-                rate=44100,
-                input=True,
-                frames_per_buffer=1024)
-
-print("* recording")
+stream = p.open(format=pyaudio.paInt16, channels=2, rate=44100, input=True, frames_per_buffer=1024)
 
 frames = []
-
 for i in range(0, int(44100 / 1024 * 5)):
     data = stream.read(1024)
     frames.append(data)
 
-print("* done recording")
-
+# Stop
 stream.stop_stream()
 stream.close()
 p.terminate()
 
+# Write file
 wf = wave.open("test.wav, 'wb')
 wf.setnchannels(2)
 wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
